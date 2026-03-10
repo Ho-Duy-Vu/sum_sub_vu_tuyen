91903a58acd83788ce5e1c0ccee09c344440f1fd
ho duy vu - 2280603726 <duyvu11092004@gmail.com>
2026-03-10 10:08:20 +0700

    Add sum function

---
+++ main.py
@@ -0,0 +1,7 @@
+#sum a,b
+def sum(a,b):
+    return a+b
+if __name__ == "__main__":
+    a = int(input("Nhap so a: "))
+    b = int(input("Nhap so b: "))
+    print("Tong cua a va b la: ", sum(a,b))
